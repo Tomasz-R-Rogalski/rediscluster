1bd7c217836ef1a0f0de6f6fda72296346df44c7
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-09-22 21:53:03 +0200

    Initial eks cluster setup with redis cluster infra

---
+++ terraform/eks.tf
@@ -0,0 +1,69 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+   #   key_name      = aws_key_pair.generated_key.key_name
+
+      tags = {
+        ExtraTag = "helloworld"
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+#variable "key_name" {}
+
+#resource "tls_private_key" "example" {
+#  algorithm = "RSA"
+#  rsa_bits  = 4096
+#}
+
+#resource "aws_key_pair" "generated_key" {
+#  key_name   = var.key_name
+#  public_key = tls_private_key.example.public_key_openssh
+#}
+
+#output "private_key" {
+#  value     = tls_private_key.example.private_key_pem
+#  sensitive = true
+#}
