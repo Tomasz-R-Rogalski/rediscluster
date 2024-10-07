4070105ea930ea724b1e5a94b665b3f74091daf1
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-10-07 23:53:39 +0200

    Update addon.tf

--- terraform/addon.tf
+++ terraform/addon.tf
@@ -2,3 +2,8 @@ resource "aws_eks_addon" "ebs-addon" {
   cluster_name = aws_eks_cluster.redis-cluster.name
   addon_name   = "aws-ebs-csi-driver"
 }
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.redis-cluster.name
+  addon_name   = "coredns"
+}
