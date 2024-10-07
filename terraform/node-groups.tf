7af06601b57a0be741482f61dc0b63ec04c35ea1
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-10-07 15:52:37 +0200

    Terraform eks change from module to resources

---
+++ terraform/node-groups.tf
@@ -0,0 +1,95 @@
+# Role for node group
+resource "aws_iam_role" "node-group-role" {
+  name = "node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2024-10-06"
+  })
+}
+
+# IAM policy attachment to nodegroup
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-group-role.name
+}
+
+
+# Node group 
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name    = aws_eks_cluster.redis-cluster.name
+  node_group_name = "private-nodes"
+  node_role_arn   = aws_iam_role.node-group-role.arn
+
+  subnet_ids = [
+    aws_subnet.private-eu-central-1a.id,
+    aws_subnet.private-eu-central-1b.id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    node = "kubenode"
+  }
+
+  # taint {
+  #   key    = "team"
+  #   value  = "devops"
+  #   effect = "NO_SCHEDULE"
+  # }
+
+  # launch_template {
+  #   name    = aws_launch_template.eks-with-disks.name
+  #   version = aws_launch_template.eks-with-disks.latest_version
+  # }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-group-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+# launch template if required
+
+# resource "aws_launch_template" "eks-with-disks" {
+#   name = "eks-with-disks"
+
+#   key_name = "local-provisioner"
+
+#   block_device_mappings {
+#     device_name = "/dev/xvdb"
+
+#     ebs {
+#       volume_size = 50
+#       volume_type = "gp2"
+#     }
+#   }
+# }
---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+# AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
