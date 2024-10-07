2f2613eedc8bd25beb6e30413057e0f3f266db0c
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-10-07 20:05:29 +0200

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "eks-cluster-role" {
 
   assume_role_policy = <<POLICY
 {
-    "Version": "2024-10-06",
+    "Version": "2012-10-17",
     "Statement": [
         {
             "Effect": "Allow",
