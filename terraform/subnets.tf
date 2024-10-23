b28a19bd2329c282b51e50a987d7b237c0aab925
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-10-23 14:12:32 +0200

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -8,7 +8,7 @@ resource "aws_subnet" "private-eu-central-1a" {
   tags = {
     Name                              = "private-eu-central-1a"
     "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
+    "kubernetes.io/cluster/redis-cluster"      = "owned"
   }
 }
 
@@ -20,7 +20,7 @@ resource "aws_subnet" "private-eu-central-1b" {
   tags = {
     Name                              = "private-eu-central-1b"
     "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
+    "kubernetes.io/cluster/redis-cluster"      = "owned"
   }
 }
 
@@ -35,7 +35,7 @@ resource "aws_subnet" "public-eu-central-1a" {
   tags = {
     Name                         = "public-eu-central-1a"
     "kubernetes.io/role/elb"     = "1" #this instruct the kubernetes to create public load balancer in these subnets
-    "kubernetes.io/cluster/demo" = "owned"
+    "kubernetes.io/cluster/redis-cluster" = "owned"
   }
 }
 
@@ -48,6 +48,6 @@ resource "aws_subnet" "public-eu-central-1b" {
   tags = {
     Name                         = "public-eu-central-1b"
     "kubernetes.io/role/elb"     = "1" #this instruct the kubernetes to create public load balancer in these subnets
-    "kubernetes.io/cluster/demo" = "owned"
+    "kubernetes.io/cluster/redis-cluster" = "owned"
   }
 }
