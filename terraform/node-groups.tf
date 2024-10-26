50d7419f2064c5c150cb493f01949fa784e1d6d9
Tomasz <83236146+Tomasz-R-Rogalski@users.noreply.github.com>
2024-10-26 02:11:55 +0200

    Update node-groups.tf

--- terraform/node-groups.tf
+++ terraform/node-groups.tf
@@ -100,6 +100,7 @@ resource "aws_eks_node_group" "private-nodes" {
    }
 
   depends_on = [
+    aws_iam_role_policy_attachment.node-group-LoadBalancerPolicy
     aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.node-group-AmazonEBSCSIDriverPolicy,
